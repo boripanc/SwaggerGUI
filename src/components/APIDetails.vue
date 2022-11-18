49ee1b7beba0e4d5b3790b46aca8bb9d2f5f7ee9
boripanc <55552862+boripanc@users.noreply.github.com>
2022-11-18 22:24:46 +0700

    modified:   src/components/APIDetails.vue

--- src/components/APIDetails.vue
+++ src/components/APIDetails.vue
@@ -1,74 +1,69 @@
 <template>
   <div class="row">
     <div class="col">
-      <div class="q-pa-md">
-        <div class="q-gutter-md" style="max-width: 600px">
-          <div class="row">
-            <div class="col">
-              <q-input v-model="apiname" label="API Name" />
-            </div>
-            <div class="col">
-              <q-input v-model="version" label="version" />
-            </div>
+      <div class="q-pa-md items-start q-gutter-md">
+        <div class="row">
+          <div class="col">
+            <q-card class="my-card bg-grey-1 text-black">
+              <q-card-section>
+                <div class="text-h6">Basic Info</div>
+              </q-card-section>
+              <q-card-section
+                ><div class="q-gutter-md" style="max-width: 600px">
+                  <div class="row">
+                    <div class="col">
+                      <q-input v-model="apiname" label="API Name" />
+                    </div>
+                    <div class="col">
+                      <q-input v-model="version" label="version" />
+                    </div>
+                  </div>
+                  <div class="row">
+                    <div class="col">
+                      <q-input v-model="description" label="description" />
+                    </div>
+                    <div class="col">
+                      <div class="q-gutter-sm">
+                        <q-checkbox dense v-model="http" label="http" />
+                        <q-checkbox dense v-model="https" label="https" />
+                      </div>
+                    </div>
+                  </div>
+                </div>
+              </q-card-section>
+            </q-card>
           </div>
-          <div class="row">
-            <div class="col">
-              <q-input v-model="description" label="description" />
-            </div>
-            <div class="col">
-              <div class="q-gutter-sm">
-                <q-checkbox dense v-model="http" label="http" />
-                <q-checkbox dense v-model="https" label="https" />
-                <q-btn label="open dialog" @click="showDialog = !showDialog" />
-                <q-dialog
-                  v-model="customDialogModel"
-                  stack-buttons
-                  prevent-close
-                  @ok="onOk"
-                  @cancel="onCancel"
-                  @show="onShow"
-                  @hide="onHide"
-                >
-                  <!-- This or use "title" prop on <q-dialog> -->
-                  <template v-slot:title>Favorite Superhero</template>
-
-                  <!-- This or use "message" prop on <q-dialog> -->
-                  <template v-slot:message
-                    >What is your superhero of choice?</template
-                  >
-
-                  <template v-slot:body>
-                    <q-field
-                      icon="account_circle"
-                      helper="We need your name so we can send you to the movies."
-                      label="Your name"
-                      :label-width="3"
-                    >
-                      <q-input v-model="name" />
-                    </q-field>
-                  </template>
-
-                  <template v-slot:buttons="props">
-                    <q-btn
-                      color="primary"
-                      label="Choose Superman"
-                      @click="choose(props.ok, 'Superman')"
-                    />
-                    <q-btn
-                      color="black"
-                      label="Choose Batman"
-                      @click="choose(props.ok, 'Batman')"
-                    />
-                    <q-btn
-                      color="negative"
-                      label="Choose Spiderman"
-                      @click="choose(props.ok, 'Spiderman')"
-                    />
-                    <q-btn flat label="No thanks" @click="props.cancel" />
-                  </template>
-                </q-dialog>
-              </div>
-            </div>
+        </div>
+        <div class="row">
+          <div class="col">
+            <q-card class="my-card bg-grey-1 text-black">
+              <q-card-section>
+                <div class="text-h6">Operation Info</div>
+              </q-card-section>
+              <q-card-section
+                ><div class="q-gutter-md" style="max-width: 600px">
+                  <div class="row">
+                    <div class="col">
+                      <q-input v-model="apiname" label="API Name" />
+                    </div>
+                    <div class="col">
+                      <q-input v-model="version" label="version" />
+                    </div>
+                  </div>
+                  <div class="row">
+                    <div class="col">
+                      <q-input v-model="description" label="description" />
+                    </div>
+                    <div class="col">
+                      <div class="q-gutter-sm">
+                        <q-checkbox dense v-model="http" label="http" />
+                        <q-checkbox dense v-model="https" label="https" />
+                      </div>
+                    </div>
+                  </div>
+                </div>
+              </q-card-section>
+            </q-card>
           </div>
         </div>
       </div>
@@ -132,45 +127,7 @@ export default {
     };
   },
   mounted() {},
-  methods: {
-    displaydata: function (props) {
-      console.log(JSON.stringify(this.form));
-      console.log(props);
-    },
-    onOk(data) {},
-
-    // when props.cancel() gets called
-    onCancel() {},
-
-    // when we show it to the user
-    onShow() {},
-
-    // when it gets hidden
-    onHide() {},
-
-    // custom handler
-    async choose(okFn, hero) {
-      if (this.name.length === 0) {
-        this.$q.dialog({
-          title: "Please specify your name!",
-          message: `Can't buy tickets without knowing your name.`,
-        });
-      } else {
-        await okFn();
-        this.$q.notify(`Ok ${this.name}, going with ${hero}`);
-      }
-    },
-  },
-  data() {
-    return {
-      customDialogModel: false,
-      form: {
-        username: null,
-        email: null,
-        password: null,
-        confirmpassword: null,
-      },
-    };
-  },
+  methods: {},
+  data() {},
 };
 </script>
